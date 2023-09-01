f8220b6b73610279aefb9acad7a624751c60cb54
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-01 13:03:35 -0300

    refactor: removed unnecessary dependency

--- src/components/notepad/Notepad.tsx
+++ src/components/notepad/Notepad.tsx
@@ -1,7 +1,18 @@
 'use client'
 
+import useNotetaking from "@/hooks/useNotetaking";
+import { ChangeEvent } from "react";
+
 
 const Notepad = () => {
+    // notetaking hook
+    const { content, setContent } = useNotetaking()
+
+    // textarea on change handler
+    const handleOnChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        setContent(event.target.value)
+    }
+
     return (
         <div className="relative box-border bg-background-notepad drop-shadow-ds-001 rounded-notepad 
         flex justify-start items-start m-auto h-[88%] lg:h-[85%] w-[95%] before:content-'' before:absolute 
@@ -10,7 +21,7 @@ const Notepad = () => {
         p-2 overflow-hidden">
             <textarea className="bg-transparent bg-paper-content bg-paper-size top-[30px] bottom-[30px] md:right-0 
             md:left-[30px] xl:left-[60px] p-2 md:pl-4 absolute outline-none resize-none leading-[31px] bg-local h-[92%] 
-            w-[95%] bg-repeat overflow-hidden overflow-y-auto text-[16px] m-auto">
+            w-[95%] bg-repeat overflow-hidden overflow-y-auto text-[16px] m-auto" onChange={handleOnChange} value={content}>
             </textarea>
         </div>
     );
--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import {
-    BiBold,
-    BiItalic,
-    BiSolidEraser,
-    BiSolidFolderOpen,
-    BiSolidSave,
-    BiUnderline,
+    BiSolidEraser, BiSolidFolderOpen, BiSolidSave
 } from "react-icons/bi";
 import { GiThorHammer } from "react-icons/gi";
 import ToolItem from "./ToolItem";
@@ -17,9 +12,6 @@ const tools = [
     { name: "Clear", icon: BiSolidEraser, id: "clear-tool" },
     { name: "Open", icon: BiSolidFolderOpen, id: "open-tool" },
     { name: "Save", icon: BiSolidSave, id: "save-tool" },
-    { name: "Bold", icon: BiBold, id: "bold-tool" },
-    { name: "Italic", icon: BiItalic, id: "italic-tool" },
-    { name: "Underline", icon: BiUnderline, id: "underline-tool" },
 ];
 
 const Toolbar = () => {
@@ -28,7 +20,7 @@ const Toolbar = () => {
 
     // tools handlers
     const handleTools = (id: string) => {
-        console.log(id);
+        console.log(id)
     }
 
     // current words and chars values
@@ -36,21 +28,21 @@ const Toolbar = () => {
     const chars = content.length
 
     return (
-        <div className="box-border bg-background-toolbar drop-shadow-ds-001 flex gap-4 justify-center items-center 
-        text-white p-1 h-14 lg:h-10 w-full z-50"
+        <div className="box-border bg-background-toolbar drop-shadow-ds-001 flex gap-4 justify-between items-center 
+        text-white p-1 px-4 h-14 lg:h-10 w-full z-50"
         >
-            <div className="box-border hidden lg:flex gap-2 justify-center items-center px-8 h-full w-fit">
+            <div className="box-border hidden lg:flex gap-2 justify-center items-center h-full w-fit">
                 <GiThorHammer className="h-[70%] w-fit" />
                 <label className="uppercase font-bold text-[16px]">
                     Thorpad
                 </label>
             </div>
-            <div className="box-border flex gap-2 justify-center items-center md:px-10 h-full w-full md:w-fit">
+            <div className="box-border flex gap-2 justify-center items-center h-full w-fit">
                 {tools.map(({ id, name, icon }) => (
                     <ToolItem key={id} id={id} name={name} icon={icon} onClick={handleTools} />
                 ))}
             </div>
-            <div className="box-border hidden md:flex gap-2 justify-center items-center px-8 h-full w-fit">
+            <div className="box-border flex gap-2 justify-center items-center h-full w-fit">
                 <ToolValues words={words} chars={chars} />
             </div>
         </div>
