475deffebb271860a06bcc8cdeac638ae6a8abee
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-01 13:38:43 -0300

    feat: footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+const Footer = () => {
+    return (
+        <footer className="box-border flex gap-6 justify-center items-center h-fit w-full">
+            <div>
+                Patrick Gunnar
+            </div>
+            <div>
+                About
+            </div>
+        </footer>
+    );
+}
+ 
+export default Footer;
