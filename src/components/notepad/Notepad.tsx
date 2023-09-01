3ee1dd41fb7f150a1622ccb8c4d8c0ae936390e7
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-31 21:30:51 -0300

    feat: added styles

--- src/components/notepad/Notepad.tsx
+++ src/components/notepad/Notepad.tsx
@@ -3,7 +3,8 @@
 
 const Notepad = () => {
     return (
-        <div>
+        <div className="box-border bg-background-notepad drop-shadow-ds-001 rounded-notepad 
+        flex justify-start items-start m-auto p-2 h-[88%] lg:h-[85%] w-[95%]">
             Notepad
         </div>
     );
