476b44e64175f0a5fc119f7f077e531dd9cd42f5
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-31 21:30:41 -0300

    feat: added classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+				"background-thorpad": "linear-gradient(to top, var(--colour--001) 0%, var(--colour--002) 100%)",
+				"background-toolbar": "linear-gradient(to top, var(--colour--003) 0%, var(--colour--004) 100%)",
+				"background-notepad": "linear-gradient(to top, var(--colour--005) 0%, var(--colour--006) 100%)"
+            },
+			dropShadow: {
+				"ds-001": "-1px 2px 3px var(--colour--004)",
+			},
+			borderRadius: {
+				"notepad": "12px"
+			}
+        },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [],
+};
+
+export default config;
