ee74d1e3a56f7c019ac148e563250db8f1fd9188
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-01 10:31:02 -0300

    feat: added z-index

--- src/components/toolbar/ToolItem.tsx
+++ src/components/toolbar/ToolItem.tsx
@@ -13,7 +13,7 @@ interface ToolItemProps {
 const ToolItem: React.FC<ToolItemProps> = ({ id, name, icon: Icon, onClick }) => {
     return (
         <button className="relative box-border flex justify-center items-center mx-auto md:mx-2 h-full aspect-square 
-        rounded-tools-radius hover:bg-[var(--colour--008)] hover:before:content-[attr(dataset-name)] 
+        rounded-tools-radius hover:bg-[var(--colour--008)] hover:before:content-[attr(dataset-name)] z-50
         hover:before:absolute hover:before:top-[125%] hover:before:py-1 hover:before:px-2 hover:before:rounded-tools-radius
         hover:before:bg-background-toolbar hover:before:h-fit hover:before:w-fit hover:before:drop-shadow-ds-001"
         onClick={() => onClick(id)} dataset-name={name}>
--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -20,10 +20,14 @@ const tools = [
 ];
 
 const Toolbar = () => {
+    // tools handlers
+    const handleTools = (id: string) => {
+        console.log(id);
+    }
+
     return (
-        <div
-            className="box-border bg-background-toolbar drop-shadow-ds-001 flex gap-4 justify-center items-center 
-        text-white p-1 h-14 lg:h-10 w-full"
+        <div className="box-border bg-background-toolbar drop-shadow-ds-001 flex gap-4 justify-center items-center 
+        text-white p-1 h-14 lg:h-10 w-full z-50"
         >
             <div className="box-border hidden lg:flex gap-2 justify-center items-center px-8 h-full w-fit">
                 <div>Logo</div>
@@ -31,7 +35,7 @@ const Toolbar = () => {
             </div>
             <div className="box-border flex gap-2 justify-center items-center md:px-10 h-full w-full md:w-fit">
                 {tools.map(({ id, name, icon }) => (
-                    <ToolItem key={id} id={id} name={name} icon={icon} onClick={() => {}} />
+                    <ToolItem key={id} id={id} name={name} icon={icon} onClick={handleTools} />
                 ))}
             </div>
             <div className="box-border hidden md:flex gap-2 justify-center items-center px-8 h-full w-fit">
