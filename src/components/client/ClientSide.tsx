c606acb7bb70a86ea73472832258c7d6be94482a
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-01 14:19:01 -0300

    feat: get data from localStorage

--- src/components/client/ClientSide.tsx
+++ src/components/client/ClientSide.tsx
@@ -3,15 +3,20 @@
 import { useEffect, useState } from "react";
 import Notepad from "../notepad/Notepad";
 import Toolbar from "../toolbar/Toolbar";
+import Cookies from "js-cookie";
+import useNotetaking from "@/hooks/useNotetaking";
 
 
 const ClientSide = () => {
+    // notetaking hook
+    const { setContent } = useNotetaking()
     // check if is the client side
-    const [isClientSide, setIsClientSide] = useState(false);
+    const [isClientSide, setIsClientSide] = useState<boolean>(false);
 
     useEffect(() => {
+        setContent(localStorage.getItem('thorpadData') ?? '')
         setIsClientSide(true);
-    }, []);
+    }, [setContent]);
 
     if(!isClientSide) return <div>Loading...</div>
 
