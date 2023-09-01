c7ef56cc5786e6d53f39434c8119b6e7765bf3f5
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-31 21:29:47 -0300

    feat: added styles

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -3,7 +3,8 @@
 
 const Toolbar = () => {
     return (
-        <div>
+        <div className="box-border bg-background-toolbar drop-shadow-ds-001 flex gap-4 justify-center items-center 
+        text-white p-1 h-14 lg:h-10 w-full">
             Toolbar
         </div>
     );
