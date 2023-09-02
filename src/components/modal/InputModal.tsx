58a732a68d12b983b1182539d9bfff385e4d51b3
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-02 15:22:09 -0300

    feat: input modal

---
+++ src/components/modal/InputModal.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import useInputModal from "@/hooks/useInputModal";
+import Modal from "./Modal";
+
+
+const InputModal = () => {
+    // modal hook
+    const { open, onClose } = useInputModal()
+
+    if(!open) return null
+
+    return (
+        <Modal onClose={onClose}>
+            Input modal
+        </Modal>
+    );
+}
+ 
+export default InputModal;
