fd8b8cedeb278cdff6627537fd5e00b981d8fb85
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-02 15:21:57 -0300

    feat: about modal

---
+++ src/components/modal/AboutModal.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import useAboutModal from "@/hooks/useAboutModal";
+import Modal from "./Modal";
+
+
+const AboutModal = () => {
+    // modal hook
+    const { open, onClose } = useAboutModal()
+
+    if(!open) return null
+
+    return (
+        <Modal onClose={onClose}>
+            About modal
+        </Modal>
+    );
+}
+ 
+export default AboutModal;
